a01e82c2942a4f099d8402d330700edf9b738857
Maxwell Rupp <maxwell@giglab.dev>
2025-11-12 01:07:17 -0500

    modified nix modules

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -1,42 +1,44 @@
-_: {
-  perSystem =
-    { config
-    , self'
-    , pkgs
-    , ...
-    }:
-    {
-      devShells.default = pkgs.mkShell {
-        name = "nushell-binary-engine-shell";
-        inputsFrom = [
-          self'.devShells.rust
-          config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
-        ];
-        packages = with pkgs; [
-          # nix stuff
-          nixd
-          nixfmt-rfc-style
-          wslu
+{ pkgs
+, inputs
+, mkEmbeddedScript
+, ...
+}:
+{
+  default = pkgs.mkShell {
+    name = "nushell-binary-engine-shell";
 
-          # rust stuff
-          rustfmt
-          clippy
-          bacon
-          config.process-compose.cargo-doc-live.outputs.package
+    # Pre-commit hooks are integrated via the git-hooks package
+    # We rely on pre-commit.nix to set up the hooks, which will be available in the environment.
 
-          # utilities
-          gitflow
+    packages = with pkgs; [
+      # nix stuff
+      nixd
+      nixfmt-rfc-style
+      wslu
 
-          # # gigdot programs
-          # inputs.gigdot.packages.${system}.quick-results
-          # inputs.gigdot.packages.${system}.upjust
-          # inputs.gigdot.packages.${system}.upspell
-          # inputs.gigdot.packages.${system}.upflake
-          # inputs.gigdot.packages.${system}.cargo-update
-        ];
-        shellHook = ''
-          echo "welcome to the rust development environment for the nushell-binary-engine package" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat 2> /dev/null;
+      # rust stuff
+      rustfmt
+      clippy
+      bacon
+      inputs.cargo-doc-live.packages.${pkgs.system}.default
+
+      # utilities
+      gitflow
+      cowsay
+      lolcat
+
+      # Example: Including the default package in the shell
+      mkEmbeddedScript
+      {
+        scriptName = "engine-test-binary";
+        scriptContent = ''
+          print "Shell is active."
         '';
-      };
-    };
+      }
+    ];
+
+    shellHook = ''
+      echo "welcome to the rust development environment for the nushell-binary-engine package" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat 2> /dev/null;
+    '';
+  };
 }
--- nix/modules/pre-commit.nix
+++ nix/modules/pre-commit.nix
@@ -1,52 +1,29 @@
-{ inputs, ... }:
-{
-  imports = [
-    (inputs.git-hooks + /flake-module.nix)
-  ];
-  perSystem = _: {
-    pre-commit.settings = {
+{ inputs
+, pkgs
+, system
+, ...
+}:
+let
+  inherit system;
+  preCommitHooks = inputs.git-hooks.lib.${system}.mkHooks {
+    inherit pkgs;
+    settings = {
       hooks = {
-        # Nix formatting and linting (from .dotfiles)
+        # Nix formatting and linting
         nixpkgs-fmt.enable = true;
         statix.enable = true;
         deadnix = {
           enable = true;
-          excludes = [ "nix/modules/template.nix" ]; # Exclude template files that might have intentional dead code
+          excludes = [ "nix/modules/template.nix" ];
         };
 
         # Rust formatting and linting
         rustfmt.enable = true;
-        clippy = {
-          enable = false; # Disabled for now, as it can be too strict for some projects
-          settings = {
-            allFeatures = true;
-            denyWarnings = true;
-          };
-        };
 
-        # Shell script linting (from .dotfiles)
-        shellcheck.enable = false;
-
-        # Markdown and YAML linting (from .dotfiles)
-        markdownlint.enable = false; # Disabled for now due to strict formatting on existing files
-        yamllint.enable = false; # Disabled for now due to strict formatting on existing files
-
-        # File cleanup (from .dotfiles)
+        # File cleanup
         end-of-file-fixer.enable = true;
-
-        # Custom flake check hook (from .dotfiles)
-        nix-flake-check-main-develop = {
-          enable = false;
-          name = "nix flake check on develop/main";
-          entry = "./scripts/pre-commit-flake-check.sh";
-          language = "script";
-          pass_filenames = false;
-          stages = [
-            "pre-commit"
-            "pre-merge-commit"
-          ];
-        };
       };
     };
   };
-}
+in
+preCommitHooks
